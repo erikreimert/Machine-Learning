1082f5b4ab4677feec408889a43ea5bded7f14a2
Erik Reimert Burro <ereimertburro@wpi.edu>
2020-04-06 20:15:46 -0400

    added meme support

--- Project 2 Age Classifier/homework2_ereimertburro_vtdemendoncafilh.py
+++ Project 2 Age Classifier/homework2_ereimertburro_vtdemendoncafilh.py
@@ -1,4 +1,5 @@
 import numpy as np
+from PIL import Image
 
 # Given an array of faces (N x M x M, where N is number of examples and M is number of pixes along each axis),
 # return a design matrix Xtilde ((M**2 + 1) x N) whose last row contains all 1s.
@@ -7,17 +8,21 @@ def reshapeAndAppend1s (faces):
 
 # Given a vector of weights w, a design matrix Xtilde, and a vector of labels y, return the (unregularized)
 # MSE.
+
+###########################################################Is it np.dot() or the multiplication
 def fMSE (w, Xtilde, y):
-    pass
+    return np.mean((Xtilde.transpose() * w -y)**2)/2
+###########################################################
 
 # Given a vector of weights w, a design matrix Xtilde, and a vector of labels y, and a regularization strength
 # alpha (default value of 0), return the gradient of the (regularized) MSE loss.
 def gradfMSE (w, Xtilde, y, alpha = 0.):
-    pass
+    pass    np.mean(Xtilde*(np.dot(np.transpose(Xtilde), w) - y)) #########How to incorporate alpha
 
 # Given a design matrix Xtilde and labels y, train a linear regressor for Xtilde and y using the analytical solution.
 def method1 (Xtilde, y):
-    pass
+    w = np.linalg.solve(Xtilde, np.transpose(Xtilde)) * Xtilde * y
+
 
 # Given a design matrix Xtilde and labels y, train a linear regressor for Xtilde and y using gradient descent on fMSE.
 def method2 (Xtilde, y):
@@ -27,6 +32,9 @@ def method2 (Xtilde, y):
 # with regularization.
 def method3 (Xtilde, y):
     ALPHA = 0.1
+    #####IMPLEMENT Gradient descent
+    w =?
+    gradfMSE(w, Xtilde, y, alpha = ALPHA)
     pass
 
 # Helper method for method2 and method3.
@@ -45,5 +53,10 @@ if __name__ == "__main__":
     w2 = method2(Xtilde_tr, ytr)
     w3 = method3(Xtilde_tr, ytr)
 
+
     # Report fMSE cost using each of the three learned weight vectors
     # ...
+
+
+    im = Image.open('img.jpg')
+    im.show('image',im)
