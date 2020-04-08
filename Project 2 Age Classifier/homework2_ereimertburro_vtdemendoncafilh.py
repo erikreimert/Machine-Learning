6898411146566598523a3d24cb7d3edfa0eb0860
Erik Reimert Burro <ereimertburro@wpi.edu>
2020-04-08 18:15:56 -0400

    yeah bitch

--- Project 2 Age Classifier/homework2_ereimertburro_vtdemendoncafilh.py
+++ Project 2 Age Classifier/homework2_ereimertburro_vtdemendoncafilh.py
@@ -4,51 +4,71 @@ from PIL import Image
 # Given an array of faces (N x M x M, where N is number of examples and M is number of pixes along each axis),
 # return a design matrix Xtilde ((M**2 + 1) x N) whose last row contains all 1s.
 def reshapeAndAppend1s (faces):
-    ones = np.ones((1, 5000))
-    faces =np.reshape(faces, (2304, 5000))
+    ones = np.ones((1, len(faces[:,0,0])))
+    faces =np.reshape(faces, (2304, len(faces[:,0,0])))
     return np.vstack((faces, ones))
 
 # Given a vector of weights w, a design matrix Xtilde, and a vector of labels y, return the (unregularized)
 # MSE.
-def fMSE (w, Xtilde, y):
-    return np.mean((Xtilde.transpose().dot(w) - y)**2)/2
 
+###########################################################Is it np.dot() or the multiplication
+def fMSE (w, Xtilde, y):
+    return np.mean((Xtilde.transpose().dot(w) -y)**2)/2
+###########################################################
 
 # Given a vector of weights w, a design matrix Xtilde, and a vector of labels y, and a regularization strength
 # alpha (default value of 0), return the gradient of the (regularized) MSE loss.
 def gradfMSE (w, Xtilde, y, alpha = 0.):
-    return np.mean((np.dot(Xtilde.transpose(), w) - y)).dot(Xtilde) + alpha*(w**2)/2
+    print("Xtilde shape: ", Xtilde.shape, "w shape: ", w.shape, "y shape: ", y.shape)
+    return    np.mean((Xtilde.transpose().dot(w) - y).dot(Xtilde)) + alpha * np.mean(w.transpose().dot(w))/2
 
 # Given a design matrix Xtilde and labels y, train a linear regressor for Xtilde and y using the analytical solution.
 def method1 (Xtilde, y):
-    ones = np.ones((1,2304))
-    Xtilde = np.vstack((Xtilde, ones))
-    Wtilde = np.linalg.solve(Xtilde.dot(np.transpose(Xtilde)), Xtilde.dot(y))
-    loss = fMSE(w,Xtilde,y)
-    print('Wtilde: ', Wtilde,"Loss: ", loss)
-    return (Wtilde, loss)
+    print(Xtilde.shape)
+    #ones = np.ones((1,2304))
+    #Xtilde =  np.vstack((Xtilde, ones))
+    Wtilde = np.linalg.solve(Xtilde.dot(Xtilde.transpose()), Xtilde.dot(y))
+    print('Wtilde: ', Wtilde, "shape: ", Wtilde.shape )
+    return Wtilde
+
 
 # Given a design matrix Xtilde and labels y, train a linear regressor for Xtilde and y using gradient descent on fMSE.
 def method2 (Xtilde, y):
-    return gradientDescent(Xtilde, y)
-
+    #Choose random starting position
+    w = np.random.randn(2305) * 0.01
+    gradientDescent(Xtilde, y)
 # Given a design matrix Xtilde and labels y, train a linear regressor for Xtilde and y using gradient descent on fMSE
 # with regularization.
 def method3 (Xtilde, y):
     ALPHA = 0.1
     #####IMPLEMENT Gradient descent
-    return gradientDescent(Xtilde, y, alpha = ALPHA)
+    w = np.random.randn(1,2305) * 0.01
+    gradientDescent(Xtilde, y, alpha = ALPHA)
 
 # Helper method for method2 and method3.
 def gradientDescent (Xtilde, y, alpha = 0.):
+    print("Xtilde shape: ",Xtilde.shape, "y shape: ", y.shape)
     EPSILON = 3e-3  # Step size aka learning rate
     T = 5000  # Number of gradient descent iterations
-    w = np.random.randn(1,2304) * 0.01
-    loss = 0
+    w = np.random.randn(2305) * 0.01
+
+
     for i in range(0,T):
         w = w - EPSILON*gradfMSE(w, Xtilde, y)
-        loss = lossArray + gradfMSE(w, Xtilde, y)
-    return (w,loss)
+
+
+    return w
+
+######TO DO WORK
+#Get W shape (1,2304)
+#Reshape to 48x48
+# US plt.imshow()
+#Get 5 most gruesome errors
+def vizualize(w):
+    w = w[:-1]
+    w.reshape((48,48))
+    plt.imshow(w)
+
 
 if __name__ == "__main__":
     # Load data
@@ -61,5 +81,10 @@ if __name__ == "__main__":
     w2 = method2(Xtilde_tr, ytr)
     w3 = method3(Xtilde_tr, ytr)
 
+
     # Report fMSE cost using each of the three learned weight vectors
     # ...
+
+
+    im = Image.open('img.jpg')
+    im.show('image',im)
